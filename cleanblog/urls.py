63a8636a51a95e39546731c6e282714ebf172320
Annas Atchia <nasatchia@gmail.com>
2023-04-19 23:18:53 +0400

    Update URL patterns

--- cleanblog/urls.py
+++ cleanblog/urls.py
@@ -22,11 +22,10 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
