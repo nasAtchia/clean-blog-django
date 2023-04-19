280a582725ae2eaeea697e42aa22390bda3f6b46
Annas Atchia <nasatchia@gmail.com>
2023-04-20 00:42:48 +0400

    Update category and tag models admin

--- blog/models_admin/category_admin.py
+++ blog/models_admin/category_admin.py
@@ -5,5 +5,5 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at',)
     list_filter = ('created_at',)
     ordering = ('name',)
-    prepopulated_fields = {'slug': ['name']}
+    prepopulated_fields = {'slug': ['name'], }
     search_fields = ('name',)
--- blog/models_admin/tag_admin.py
+++ blog/models_admin/tag_admin.py
@@ -5,5 +5,5 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at',)
     list_filter = ('created_at',)
     ordering = ('name',)
-    prepopulated_fields = {'slug': ['name']}
+    prepopulated_fields = {'slug': ['name'], }
     search_fields = ('name',)
