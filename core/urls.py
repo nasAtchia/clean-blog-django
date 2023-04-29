b2bfe029bc49b22486b4230632b652e93830d2f7
Annas Atchia <nasatchia@gmail.com>
2023-04-29 10:38:46 +0400

    WIP contact page

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 
 from .views.about import AboutView
+from .views import contact
 
 app_name = 'core'
 
 
 urlpatterns = [
     path('about/', AboutView.as_view(), name='about'),
+    path('contact/', contact.index, name='contact'),
 ]
