3fc65bb48eb47545d95390886b45c06ed5a582da
Annas Atchia <nasatchia@gmail.com>
2023-04-29 10:30:36 +0400

    Update nav

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+
 from .views import category, post, tag
 
 app_name = 'blog'
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
+
 from .views.about import AboutView
 
 app_name = 'core'
 
 
 urlpatterns = [
-    path('about/', AboutView.as_view()),
+    path('about/', AboutView.as_view(), name='about'),
 ]
